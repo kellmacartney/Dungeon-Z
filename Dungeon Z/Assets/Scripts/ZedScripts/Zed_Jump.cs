b44ba91911d8dad0045bb645c2ee02b963d234c3
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2018-03-20 10:16:31 +1000

    initla push
    inital push with the following:
    Scene set up
    Player game object and scripts

---
+++ Dungeon Z/Assets/Scripts/ZedScripts/Zed_Jump.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zed_Jump : MonoBehaviour
+{
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    public float jumpVelocity; 
+
+    Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+   
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            Jump();
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+       
+        
+	}
+
+    void Jump()
+    {
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+        }
+    }
+   
+}
